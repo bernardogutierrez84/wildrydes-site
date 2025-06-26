6b3727051b0267dca373c435019c9a71957fb08d
Bernardo-Data <valantaine84@gmail.com>
2025-06-26 14:15:56 -0500

    actualizar cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_RZnraTt6E', 
-        userPoolClientId: '490fgrreikgdjjrj6d2je40nf4', 
+        userPoolId: 'us-east-2_xEhogv9OP', 
+        userPoolClientId: '4o6mu5jbn82hjqd6tl8is1ik3q', 
         region: 'us-east-2' 
     },
     api: {
